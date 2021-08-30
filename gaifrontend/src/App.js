f570ded755e36e6298092a4c22a9bb47baa17e61
guru <gurudev.c@gai.co.in>
2021-08-30 17:27:36 +0530

    Add gaifrontend folder

---
+++ gaifrontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Home from './Home';
+import Contact from './Contact';
+import About from './About'; 
+import Services from './Services';
+import Navbar from './Navbar';
+import { Switch, Route, Redirect } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/services" component={Services} />
+        <Route exact path="/contact" component={Contact} />
+        <Redirect to="/" />
+      </Switch>
+    </>
+  );
+}
+
+export default App;
---
+++ gaifrontend/src/Sdata.jsx
@@ -0,0 +1,30 @@
+import web from "../src/images/img1.jpg";
+
+const Sdata = [
+    {
+        imgsrc: web,
+        title: "Web development",
+    },
+    {
+        imgsrc: web,
+        title: "App development",
+    },
+    {
+        imgsrc: web,
+        title: "Software development",
+    },
+    {
+        imgsrc: web,
+        title: "Web development",
+    },
+    {
+        imgsrc: web,
+        title: "Digital development",
+    },
+    {
+        imgsrc: web,
+        title: "Android development",
+    },
+]
+
+export default Sdata;
