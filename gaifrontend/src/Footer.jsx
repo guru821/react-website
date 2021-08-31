57483a2ae6fcdf6a4f98a7d72e4e94e7cd231385
guru <gurudev.c@gai.co.in>
2021-08-31 12:50:37 +0530

    Modify the structure

---
+++ gaifrontend/src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="w-100 bg-light text-center mt-5">
+                <p className="mb-0">copyright 2021</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
